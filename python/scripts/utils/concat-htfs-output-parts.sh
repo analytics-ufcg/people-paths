7180d82585b7a7c7329fc1959bff377b3def1b1b
Tarciso Braz <tarcisocomp@gmail.com>
2019-02-06 19:19:29 -0200

    Adding utils scripts to concat hdfs and normal fs files.

---
+++ python/scripts/utils/concat-htfs-output-parts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+if [ "$#" -lt 2 ]; then
+    echo "Wrong number of parameters"
+	echo "Usage:"
+	echo "./concat-hdfs-output-parts.sh <input-folderpath> <output-folderpath>"
+
+else
+
+	input_folderpath=$1
+	output_folderpath=$2
+
+	for f in $input_folderpath/*
+	do
+		subfoldername=${f##*/} # will drop begin of string upto last occur of `SubStr`
+		echo "Processing date: $subfoldername - located at: $f"
+		output_subfolderpath=$output_folderpath/$subfoldername.csv
+		
+		random_file=`ls $f | shuf -n 1`
+		files_header=`head -1 $f/$random_file`
+		
+		echo $files_header > $output_subfolderpath		
+		tail -q -n +2 $f/part-* >> $output_subfolderpath
+	done
+
+	echo "Done!"
+
+fi
